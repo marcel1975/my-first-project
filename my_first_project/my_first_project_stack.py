ec32a7a365b1677d4faa59f7362d8348721b996e
Marcin Walkowski <marcin.walkowski@partner.bmw.de>
2024-04-20 18:59:53 +0200

    metadata versioning disabled

--- my_first_project/my_first_project_stack.py
+++ my_first_project/my_first_project_stack.py
@@ -1,7 +1,10 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    aws_s3 as _s3
+    aws_s3 as _s3,
+    aws_iam as _iam,
+    CfnOutput,
+    Token
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -23,6 +26,32 @@ class MyFirstProjectStack(Stack):
             self, 
             "MyFirstProjectBucket",
             bucket_name="my-first-project-bucket-12345",
-            versioned=True,
-            encryption=_s3.BucketEncryption.KMS_MANAGED
+            versioned=False,
+            encryption=_s3.BucketEncryption.S3_MANAGED,
+            block_public_access=_s3.BlockPublicAccess.BLOCK_ALL
+        )
+
+        mybucket = _s3.Bucket(
+            self,
+            "myBucketid"
+        )
+
+        snstopicname = "mySNSTopic"
+
+        if not Token.is_unresolved(snstopicname) and len(snstopicname) > 10:
+            raise ValueError("Maximum length of SNS topic name must be 10 or less")
+
+        print(mybucket.bucket_name)
+
+        _iam.Group(
+            self,
+            "gid"
+        )
+
+        output_1 = CfnOutput(
+            self,
+            "myBucketOutput1",
+            value=mybucket.bucket_name,
+            description=f"My first CDK bucket",
+            export_name="myBucketOutput1"
         )
